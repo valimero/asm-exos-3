b86aa6e5feea69185ed822f6c19431405e46cd13
Valentin Coppin <valentin.coppin@ecole2600.com>
2021-09-09 13:41:36 +0200

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# To run in your root directory
+mkdir my_strdup my_strrev my_fillstruct my_whereami
+touch ./my_strdup/my_strdup.s ./my_strrev/my_strrev.s ./my_fillstruct/my_fillstruct.s ./my_whereami/my_whereami.s
